75507a102e4c3b51f72814edfaf931667e0a5def
rafael <rafinhamattos486@gmail.com>
2025-05-20 21:05:40 -0300

    Adição da const image e nunber

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 const pokemomname = document.querySelector(".pokemom_name")
+const pokemomnunber = document.querySelector(".pokemom_nunber")
+const pokemomimage = document.querySelector(".pokemom_image")
+
 const fetchpokemom = async (pokemom) => {
   const APIResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemom}`
   )
-  const data = await APIResponse.json();
-  return data;
+  const data = await APIResponse.json()
+  return data
 }
 
 const renderPokemom = async (pokemom) => {
   const data = await fetchpokemom(pokemom)
 
-  pokemomname.innerHTML = data.name;
+  pokemomname.innerHTML = data.name
+  pokemomnunber.innerHTML = data.id
+  pokemomimage.src =
+    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_default"
+    ]
 }
 
-renderPokemom('90')
+renderPokemom("649")
